4f85634d41aa0a781eaad05303868d6cfe6116d0
Steve <stevesough@gmail.com>
2018-02-21 14:08:57 +0800

    api结构改善

---
+++ resources/jw.py
@@ -0,0 +1,30 @@
+from flask_restful import reqparse
+from flask.ext import restful
+
+def post_data():
+    parser = reqparse.RequestParser()
+    parser.add_argument('username' , required=True,type=str)
+    parser.add_argument('password', required=True,type=str)
+    args = parser.parse_args()
+    return args
+
+class grade_all(restful.Resource):
+    def post(self):
+        args = post_data()
+        app = grades(args['username'],args['password'],methond='all')
+        res=app.main()
+        return jsonify(res)
+
+class grade_year(restful.Resource):
+    def post(self):
+        args = post_data()
+        app = grades(args['username'],args['password'],methond='xn')
+        res=app.main()
+        return jsonify(res)
+
+class grade_term(restful.Resource):
+    def post(self):
+        args = post_data()
+        app = grades(args['username'],args['password'],methond='xn')
+        res=app.main()
+        return jsonify(res)
